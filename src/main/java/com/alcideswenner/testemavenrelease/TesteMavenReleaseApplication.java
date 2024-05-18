0938a6ba4cde08730fbb249b0b986d14137b72c2
Alcides Wenner <alcides.wenner@itspossiblefor.me>
2024-05-17 21:47:38 -0300

    create project

---
+++ src/main/java/com/alcideswenner/testemavenrelease/TesteMavenReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.testemavenrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMavenReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMavenReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alcideswenner/testemavenrelease/TesteMavenReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.testemavenrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMavenReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
